63cd081e073d4251836015c4f89cbfbd2583ead4
金田 明浩 <kaneda@macbook.local>
2012-07-30 22:08:35 +0900

    Initial Commit

---
+++ keigeki/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  keigeki
+//
+//  Created by 金田 明浩 on 2012/07/30.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}

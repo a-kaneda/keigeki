4b5cce511521d2e8877a4409b13926ecac301269
A.Kaneda <kaneda65@gmail.com>
2013-02-18 14:05:30 +0900

    ライセンス表記を追加。

--- keigeki/AKNavigationController.m
+++ keigeki/AKNavigationController.m
@@ -1,3 +1,31 @@
+/*
+ * Copyright (c) 2012-2013 Akihiro Kaneda.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *   1.Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   2.Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *   3.Neither the name of the Monochrome Soft nor the names of its contributors
+ *     may be used to endorse or promote products derived from this software
+ *     without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 /*!
  @file AKNavigationController.m
  @brief UINavigationControllerのカスタマイズ
@@ -17,10 +45,6 @@
 static NSString *kAKAplUrl = @"https://itunes.apple.com/us/app/qing-ji/id569653828?l=ja&ls=1&mt=8";
 /// AdMobパブリッシャーID
 static NSString *kAKAdMobID = @"0fc25f9edb6a4772";
-/// テストデバイス(iPhone4)のID
-static NSString *kAKTestDeviceIPhone4ID = @"764805932506a86afa8091da68200eab3fb177b9";
-/// テストデバイス(iPhone5)のID
-static NSString *kAKTestDeviceIPhone5ID = @"59d89c955b8adbe31a45ec3f07ad5ea813b11c24";
 
 /*!
  @brief UINavigationControllerのカスタマイズ
@@ -186,12 +210,8 @@ static NSString *kAKTestDeviceIPhone5ID = @"59d89c955b8adbe31a45ec3f07ad5ea813b1
     self.bannerView.rootViewController = self;
     [self.view addSubview:self.bannerView];
     
-    // テスト広告の設定をする
+    // 広告リクエストを作成する
     GADRequest *request = [GADRequest request];
-    request.testing = YES;
-    request.testDevices = [NSArray arrayWithObjects:GAD_SIMULATOR_ID, kAKTestDeviceIPhone4ID, kAKTestDeviceIPhone5ID, nil];
-    
-    AKLog(1, @"testDevices=%@", request.testDevices);
     
     // リクエストを行って広告を読み込む
     [self.bannerView loadRequest:request];
